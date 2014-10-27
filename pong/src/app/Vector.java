e4fb510dca118288afdb3de42003fcdd8bd5ef38
Timo <timo@Penis.local>
2014-10-27 22:27:29 +0100

    added Vector

--- pong/src/app/Vector.java
+++ pong/src/app/Vector.java
@@ -1,7 +1,33 @@
 package app;
 
 public class Vector {
-	private float _vX = 0;
-	private float _vY = 0;
+	float _vX,
+		  _vY;
 	
+	public Vector() {
+		_vX = 0;
+		_vY = 0;
+	}
+	
+	public Vector(float vX, float vY) {
+		_vX = vX;
+		_vY  = vY;
+	}
+
+	
+	public float getVX() {
+		return _vX;
+	}
+
+	public void setVX(float vX) {
+		_vX = vX;
+	}
+
+	public float getVY() {
+		return _vY;
+	}
+
+	public void setVY(float vY) {
+		_vY = vY;
+	}
 }
